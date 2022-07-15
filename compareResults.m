07435216f4455d2fd8bc3afb4c48e1fb603d8c54
Adria <avillacrosa@gmail.com>
2022-07-15 09:39:22 +0200

    Go

--- compareResults.m
+++ compareResults.m
@@ -5,7 +5,7 @@ for t = 1:10
 	mine = fullfile(myDisptracF, sprintf("DISPTRAC_%02i.txt",t-1));
 	theirs = fullfile(shrDisptracF, sprintf("DISPTRAC__TEST_%02i.txt",t));
 	plotDisptrac(theirs, 'Shrine', 1);
-	plotDisptrac(mine, 'Adaptation', 1e6);
+	plotDisptrac(mine, 'Adaptation', 1);
 % 	close all;
 	return
 end
--- src/postprocessing/writeDisptrac.m
+++ src/postprocessing/writeDisptrac.m
@@ -1,20 +1,19 @@
 function writeDisptrac(u, t, Geo, fname)
     [~, top_idx] = ext_z(0, Geo);
-    ftfm  = fopen(fname,'w+');
-    x  = vec_to_tfmvec(Geo.X(top_idx, 1), Geo); x = x(:);
-    y  = vec_to_tfmvec(Geo.X(top_idx, 2), Geo); y = y(:);
+%     ftfm  = fopen(fname,'w+');
+    x  = vec_to_tfmvec(Geo.X(top_idx, 1), Geo);
+    y  = vec_to_tfmvec(Geo.X(top_idx, 2), Geo);
 % 	vecu = vec_to_tfmvec([u(top_idx, 1), u(top_idx, 2)], Geo);
-    vecux = vec_to_tfmvec(u(top_idx, 1), Geo);
-    vecuy = vec_to_tfmvec(u(top_idx, 2), Geo);
+%     vecux = vec_to_tfmvec(u(top_idx, 1), Geo);
+%     vecuy = vec_to_tfmvec(u(top_idx, 2), Geo);
+    vecux = u(top_idx, 1);
+    vecuy = u(top_idx, 2);
 	
-    vectx = vec_to_tfmvec(t(top_idx, 1), Geo);
-    vecty = vec_to_tfmvec(t(top_idx, 2), Geo);
-%     x  = Geo.X(top_idx, 1);
-%     y  = Geo.X(top_idx, 2);
-%     vecux = u(top_idx, 1);
-%     vecuy = u(top_idx, 2);
-%     vectx = t(top_idx, 1);
-%     vecty = t(top_idx, 2);
-    fprintf(ftfm,'%d %d %d %d %d %d \n', [x'; y'; vecux'; vecuy'; vectx'; vecty']);
-    fclose(ftfm);
+    vectx = t(top_idx, 1);
+    vecty = t(top_idx, 2);
+
+    disptrac = [x, y, vecux, vecuy, vectx, vecty]; 
+
+    save(fname, 'disptrac','-ascii');
+%     fclose(ftfm);
 end
